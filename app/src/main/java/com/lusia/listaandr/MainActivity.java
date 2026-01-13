883be9416b379cde006d9d3c499e13b8b3d13c63
KoffeCraft <lucycraft.minecrafte@gmail.com>
2026-01-13 09:02:51 +0100

    przekreślanie i usuwanie

--- app/src/main/java/com/lusia/listaandr/MainActivity.java
+++ app/src/main/java/com/lusia/listaandr/MainActivity.java
@@ -2,11 +2,15 @@ package com.lusia.listaandr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -28,6 +32,36 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(arrayAdapter);
+
+
+
+        listView.setOnItemClickListener(
+            new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    TextView textView = (TextView) view;
+                    if(textView.getPaintFlags() != Paint.STRIKE_THRU_TEXT_FLAG) {
+                        view.setBackgroundColor(Color.GRAY);
+                        textView.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+                    }
+                    else {
+                        view.setBackgroundColor(Color.WHITE);
+                        textView.setPaintFlags(0);
+                    }
+                }
+            }
+        );
+
+        listView.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        noteList.remove(i);
+                        arrayAdapter.notifyDataSetChanged();
+                        return false;
+                    }
+                }
+        );
     }
 
     public void onClick(View view) {
@@ -37,4 +71,5 @@ public class MainActivity extends AppCompatActivity {
         noteList.add(note);
         arrayAdapter.notifyDataSetChanged();
     }
+
 }
